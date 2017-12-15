4379e1ecd063fc232292a490be3d0ce58e232910
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2017-12-15 00:58:24 -0500

    fixing pickling issue

--- Code/prepQAvocabs.py
+++ Code/prepQAvocabs.py
@@ -1,36 +1,13 @@
 from __future__ import unicode_literals, print_function, division
 from io import open
 import unicodedata
-import json
+from shared_util import Lang
 import re
 import random
 import pickle
 
-
 SOS_token = 0
 EOS_token = 1
-
-
-class Lang:
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {0: "SOS", 1: "EOS", 2: "UNK"}
-        self.n_words = 3  # Count SOS and EOS
-
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
-
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
 
 
 ######################################################################
@@ -159,7 +136,7 @@ if __name__ == "__main__":
     train_file_in = data_path + "train.txt"
     valid_file_in = data_path + "valid.txt"
     test_file_in = data_path + "test.txt"
-
+    Lang.__module__="shared_util"
     print("Creating dictionary for train...")
     print("Processing Training data...")
     source_path = data_path + "train_source.pickle"
